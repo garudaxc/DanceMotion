527193f361b9635a1c9132d5668a896b67037bca
Garuda <181031952@qq.com>
2020-10-16 19:40:15 +0800

    add line between bones

--- MotionGraph/visualtest.py
+++ MotionGraph/visualtest.py
@@ -1,29 +1,40 @@
 from vpython import *
+import fbx
 import skeleton
 
-ball = sphere(pos=vector(-5,0,0), radius=0.5,color=color.cyan, make_trail=True, retain=200)
-wallR = box(pos=vector(6,0,0), size=vector(0.2,12,12), color=color.green)
-ball.velocity = vector(25,0,0)
-deltat = 0.05
-t = 0
-ball.pos = ball.pos + ball.velocity*deltat
-while True:
-    rate(200)
-    ball.velocity.x = -ball.velocity.x
-    ball.pos = ball.pos + ball.velocity*deltat
-    t = t + deltat 
 
-# def DrawJoints(skel):
-#     size = len(skel)
-#     x = np.zeros(size)
-#     y = np.zeros(size)
-#     z = np.zeros(size)
-#     for i, bone in enumerate(skel):
-#         pos = bone.mWorld_.GetT()
-#         x[i], y[i], z[i] = pos[0], pos[1], pos[2]
+def ToVector(self):
+    return vector(self[0], self[1], self[2])
+
+setattr(fbx.FbxVector4, 'ToVector', ToVector)
+
+
+scene.caption = """To rotate "camera", drag with right button or Ctrl-drag.
+To zoom, drag with middle button or Alt/Option depressed, or use scroll wheel.
+  On a two-button mouse, middle is left + right.
+To pan left/right and up/down, Shift-drag.
+Touch screen: pinch/extend to zoom, swipe or two-finger rotate."""
+
+
+
+def DrawJoints(skel):
+    size = len(skel)
+    for i, bone in enumerate(skel):
+        pos = bone.mWorld_.GetT().ToVector()
+        s = sphere(pos=pos, pos[2]), radius=1)
+
+    for i in range(1, size):
+        p0 = skel[i].mWorld_.GetT().ToVector()
+        p = skel[i].parent_
+        p1 = skel[p].mWorld_.GetT().ToVector()
+
+        c = curve(p0, p1)
         
-#     #print(x, y)
-#     mlab.points3d(x, y, z, colormap="copper", scale_factor=2)
+    #print(x, y)
+    #mlab.points3d(x, y, z, colormap="copper", scale_factor=2)
+
+
+
 
 
 
@@ -33,4 +44,6 @@ if __name__ == '__main__':
     path = '''resource/test.FBX'''
     path = '''resource/MrChu/MrChu.FBX'''
 
-    # skel = skeleton.load_skeleton(path)
+    skel = skeleton.load_skeleton(path)
+
+    DrawJoints(skel)
